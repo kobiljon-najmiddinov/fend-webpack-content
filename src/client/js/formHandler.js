164d1ae49560a988f6cef858b5b6031aa6a04552
Kobiljon <qobiljonnajmiddinov@gmail.com>
2023-06-13 21:57:05 +0500

    babel, webpack loaders

--- src/client/js/formHandler.js
+++ src/client/js/formHandler.js
@@ -1,4 +1,4 @@
-function handleSubmit(event) {
+export function handleSubmit(event) {
     event.preventDefault()
 
     // check what text was put into the form field
@@ -6,9 +6,9 @@ function handleSubmit(event) {
     checkForName(formText)
 
     console.log("::: Form Submitted :::")
-    fetch('http://localhost:8080/test')
-    .then(res => res.json())
-    .then(function(res) {
-        document.getElementById('results').innerHTML = res.message
-    })
+    fetch('http://localhost:8000/test')
+        .then(res => res.json())
+        .then(function (res) {
+            document.getElementById('results').innerHTML = res.message
+        })
 }
